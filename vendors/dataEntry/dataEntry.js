ce644d1ac01cfd4defb393da0e6d5049c18c953c
161250059 <161250059@smail.nju.edu.cn>
2018-06-10 13:43:13 +0800

    数据录入（完成） 表格美化

---
+++ vendors/dataEntry/dataEntry.js
@@ -0,0 +1,66 @@
+
+
+var nodeType ="harvest";
+
+
+if(nodeType==="harvest"){
+    document.getElementById('process').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('produce').remove();
+    document.getElementById('sale').remove();
+}
+else if(nodeType=="process"){
+    document.getElementById('harvest').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('produce').remove();
+    document.getElementById('sale').remove();
+}
+else if(nodeType=="storage"){
+    document.getElementById('harvest').remove();
+    document.getElementById('process').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('produce').remove();
+    document.getElementById('sale').remove();
+
+}
+else if(nodeType=="examine"){
+    document.getElementById('harvest').remove();
+    document.getElementById('process').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('produce').remove();
+    document.getElementById('sale').remove();
+
+}
+else if(nodeType=="purchase"){
+    document.getElementById('harvest').remove();
+    document.getElementById('process').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('produce').remove();
+    document.getElementById('sale').remove();
+
+}
+else if(nodeType=="produce"){
+    document.getElementById('harvest').remove();
+    document.getElementById('process').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('sale').remove();
+
+}
+else if(nodeType=="sale"){
+    document.getElementById('harvest').remove();
+    document.getElementById('process').remove();
+    document.getElementById('storage').remove();
+    document.getElementById('examine').remove();
+    document.getElementById('purchase').remove();
+    document.getElementById('produce').remove();
+
+}
