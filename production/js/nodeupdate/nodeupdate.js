3cf19365d4de201232316b40295c125796c6d313
161250095 <161250095@smail.nju.edu.cn>
2018-06-12 23:41:58 +0800

    some changes

--- production/js/nodeupdate/nodeupdate.js
+++ production/js/nodeupdate/nodeupdate.js
@@ -10,5 +10,19 @@ function summit(){
     console.log(companyName);
 }
 
+var find = "SELECT * from company where username = 'user'";
+connection.query(find, function(error, results, fields){
+    if (error) {
+        throw error;
+    }
+    if (results) {
+        for(var i = 0; i < results.length; i++)
+        {
+            console.log('%s\t%s',results[i].username,results[i].pass_word);
+            $('#produceID').val();
+        }
+    }
+});
+
 
 
