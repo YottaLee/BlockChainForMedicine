6c6a0fc49b17569e9c3a3069473492806fcb8d76
161250059 <161250059@smail.nju.edu.cn>
2018-06-12 19:01:35 +0800

    三链改成一链（文字）

--- vendors/dataEntry/dataEntry.js
+++ vendors/dataEntry/dataEntry.js
@@ -1,59 +1,20 @@
 
 
-var nodeType ="harvest";
+var nodeType ="produce";
 
 
-if(nodeType==="harvest"){
-    document.getElementById('process').remove();
+if(nodeType==="grow"){
     document.getElementById('storage').remove();
     document.getElementById('examine').remove();
     document.getElementById('purchase').remove();
     document.getElementById('produce').remove();
     document.getElementById('sale').remove();
-}
-else if(nodeType=="process"){
-    document.getElementById('harvest').remove();
-    document.getElementById('storage').remove();
-    document.getElementById('examine').remove();
-    document.getElementById('purchase').remove();
-    document.getElementById('produce').remove();
-    document.getElementById('sale').remove();
-}
-else if(nodeType=="storage"){
-    document.getElementById('harvest').remove();
-    document.getElementById('process').remove();
-    document.getElementById('examine').remove();
-    document.getElementById('purchase').remove();
-    document.getElementById('produce').remove();
-    document.getElementById('sale').remove();
-
-}
-else if(nodeType=="examine"){
-    document.getElementById('harvest').remove();
-    document.getElementById('process').remove();
-    document.getElementById('storage').remove();
-    document.getElementById('purchase').remove();
-    document.getElementById('produce').remove();
-    document.getElementById('sale').remove();
-
-}
-else if(nodeType=="purchase"){
-    document.getElementById('harvest').remove();
-    document.getElementById('process').remove();
-    document.getElementById('storage').remove();
-    document.getElementById('examine').remove();
-    document.getElementById('produce').remove();
-    document.getElementById('sale').remove();
-
 }
 else if(nodeType=="produce"){
     document.getElementById('harvest').remove();
     document.getElementById('process').remove();
     document.getElementById('storage').remove();
-    document.getElementById('examine').remove();
-    document.getElementById('purchase').remove();
     document.getElementById('sale').remove();
-
 }
 else if(nodeType=="sale"){
     document.getElementById('harvest').remove();
@@ -62,5 +23,5 @@ else if(nodeType=="sale"){
     document.getElementById('examine').remove();
     document.getElementById('purchase').remove();
     document.getElementById('produce').remove();
-
 }
+
