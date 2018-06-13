2faf0940bb86594c32ad0ab9eb6479687b1f245c
161250095 <161250095@smail.nju.edu.cn>
2018-06-13 16:59:09 +0800

    make some changes

--- production/js/block_info/block_info.js
+++ production/js/block_info/block_info.js
@@ -1,11 +1,13 @@
 var currentblock;
-var blockArr = [];
+var blockArr = [{value:23,height:0,size:"1.01M",time:"2018-06-10 12:30:45",place:"宁夏中宁宁安堡种植基地",hash:"353532e3b0c44298fc1c149afbf4c8ibt7892427ae41e4649b934ca4kybjtg7852b855"},
+    {value:12,height:1,size:"2.01M",time:"2018-06-11 14:23:11",place:"宁夏中宁宁安堡种植基地",hash:"353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991bjbds855"},
+    {value:32,height:2,size:"1.01M",time:"2018-06-12 13:04:12",place:"河南辅仁堂制药有限公司",hash:"353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca4kybjtg7852b855"},
+    {value:55,height:3,size:"3.01M",time:"2018-06-13 12:11:23",place:"河南辅仁堂制药有限公司",hash:"353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca4kpokj87852b855"},
+    {value:15,height:4,size:"2.23M",time:"2018-06-13 15:44:09",place:"青海诺木洪种植基地",hash:"353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b90o9i8kybjtg7852b855"},
+    {value:17,height:5,size:"1.25M",time:"2018-06-13 15:49:45",place:"青海诺木洪种植基地",hash:"353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934okj9fbjtg7852b855"}
+];
 var index = -1;
-
-function test(ev) {
-    console.log(ev.id)
-}
-
+document.getElementById("block0").click();
 $(".changecolor").click(function () {
 
     $(".changecolor").removeClass('chosen');
@@ -17,6 +19,8 @@ $(".changecolor").click(function () {
 function change_color(current) {
     var str = current.id;
     index = parseInt(str[str.length-1]);
+    console.log(index);
+    console.log(blockArr[index]);
     displayData(blockArr[index]);
     // var id = "#" + str;
     // console.log(id);
@@ -61,5 +65,11 @@ function nextblock() {
 }
 
 function displayData(block) {
+    document.getElementById("currentValue").innerHTML = block.value;
+    document.getElementById("height").innerHTML = block.height;
+    document.getElementById("size").innerHTML = block.size;
+    document.getElementById("time").innerHTML = block.time;
+    document.getElementById("place").innerHTML = block.place;
+    document.getElementById("hash").innerHTML = block.hash;
 
 }
