af835633a34c7b3389e536968335de09783d37ea
161250059 <161250059@smail.nju.edu.cn>
2018-06-13 10:27:07 +0800

    修复statistic冲突

--- vendors/graph/statistic.js
+++ vendors/graph/statistic.js
@@ -1,30 +1,292 @@
-if ($('#mybarChart').length ){
+var theme = {
+    color: [
+        '#26B99A', '#34495E', '#BDC3C7', '#3498DB',
+        '#9B59B6', '#8abb6f', '#759c6a', '#bfd3b7'
+    ],
 
-  var ctx = document.getElementById("mybarChart");
-  var mybarChart = new Chart(ctx, {
-	type: 'bar',
-	data: {
-	  labels: ["January", "February", "March", "April", "May", "June", "July"],
-	  datasets: [{
-		label: '# of Votes',
-		backgroundColor: "#26B99A",
-		data: [51, 30, 40, 28, 92, 50, 45]
-	  }, {
-		label: '# of Votes',
-		backgroundColor: "#03586A",
-		data: [41, 56, 25, 48, 72, 34, 12]
-	  }]
-	},
+    title: {
+        itemGap: 8,
+        textStyle: {
+            fontWeight: 'normal',
+            color: '#408829'
+        }
+    },
 
-	options: {
-	  scales: {
-		yAxes: [{
-		  ticks: {
-			beginAtZero: true
-		  }
-		}]
-	  }
-	}
-  });
+    dataRange: {
+        color: ['#1f610a', '#97b58d']
+    },
 
-}
+    toolbox: {
+        color: ['#408829', '#408829', '#408829', '#408829']
+    },
+
+    tooltip: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        axisPointer: {
+            type: 'line',
+            lineStyle: {
+                color: '#408829',
+                type: 'dashed'
+            },
+            crossStyle: {
+                color: '#408829'
+            },
+            shadowStyle: {
+                color: 'rgba(200,200,200,0.3)'
+            }
+        }
+    },
+
+    dataZoom: {
+        dataBackgroundColor: '#eee',
+        fillerColor: 'rgba(64,136,41,0.2)',
+        handleColor: '#408829'
+    },
+    grid: {
+        borderWidth: 0
+    },
+
+    categoryAxis: {
+        axisLine: {
+            lineStyle: {
+                color: '#408829'
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: ['#eee']
+            }
+        }
+    },
+
+    valueAxis: {
+        axisLine: {
+            lineStyle: {
+                color: '#408829'
+            }
+        },
+        splitArea: {
+            show: true,
+            areaStyle: {
+                color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: ['#eee']
+            }
+        }
+    },
+    timeline: {
+        lineStyle: {
+            color: '#408829'
+        },
+        controlStyle: {
+            normal: {color: '#408829'},
+            emphasis: {color: '#408829'}
+        }
+    },
+
+    k: {
+        itemStyle: {
+            normal: {
+                color: '#68a54a',
+                color0: '#a9cba2',
+                lineStyle: {
+                    width: 1,
+                    color: '#408829',
+                    color0: '#86b379'
+                }
+            }
+        }
+    },
+    map: {
+        itemStyle: {
+            normal: {
+                areaStyle: {
+                    color: '#ddd'
+                },
+                label: {
+                    textStyle: {
+                        color: '#c12e34'
+                    }
+                }
+            },
+            emphasis: {
+                areaStyle: {
+                    color: '#99d2dd'
+                },
+                label: {
+                    textStyle: {
+                        color: '#c12e34'
+                    }
+                }
+            }
+        }
+    },
+    force: {
+        itemStyle: {
+            normal: {
+                linkStyle: {
+                    strokeColor: '#408829'
+                }
+            }
+        }
+    },
+    chord: {
+        padding: 4,
+        itemStyle: {
+            normal: {
+                lineStyle: {
+                    width: 1,
+                    color: 'rgba(128, 128, 128, 0.5)'
+                },
+                chordStyle: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    }
+                }
+            },
+            emphasis: {
+                lineStyle: {
+                    width: 1,
+                    color: 'rgba(128, 128, 128, 0.5)'
+                },
+                chordStyle: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    }
+                }
+            }
+        }
+    },
+    gauge: {
+        startAngle: 225,
+        endAngle: -45,
+        axisLine: {
+            show: true,
+            lineStyle: {
+                color: [[0.2, '#86b379'], [0.8, '#68a54a'], [1, '#408829']],
+                width: 8
+            }
+        },
+        axisTick: {
+            splitNumber: 10,
+            length: 12,
+            lineStyle: {
+                color: 'auto'
+            }
+        },
+        axisLabel: {
+            textStyle: {
+                color: 'auto'
+            }
+        },
+        splitLine: {
+            length: 18,
+            lineStyle: {
+                color: 'auto'
+            }
+        },
+        pointer: {
+            length: '90%',
+            color: 'auto'
+        },
+        title: {
+            textStyle: {
+                color: '#333'
+            }
+        },
+        detail: {
+            textStyle: {
+                color: 'auto'
+            }
+        }
+    },
+    textStyle: {
+        fontFamily: 'Arial, Verdana, sans-serif'
+    }
+};
+
+var echartPie = echarts.init(document.getElementById('echart_pie'), theme);
+echartPie.clear();
+echartPie.setOption({
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} {b} : {c} ({d}%)"
+    },
+    legend: {
+        x: 'center',
+        y: 'bottom',
+        data: ['合格', '不合格']
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            magicType: {
+                show: true,
+                type: ['pie', 'funnel'],
+                option: {
+                    funnel: {
+                        x: '25%',
+                        width: '50%',
+                        funnelAlign: 'left',
+                        max: 6
+                    }
+                }
+            },
+            restore: {
+                show: true,
+                title: "Restore"
+            },
+            saveAsImage: {
+                show: true,
+                title: "Save Image"
+            }
+        }
+    },
+    calculable: true,
+    series: [{
+        name: '访问来源',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '48%'],
+        data: [{
+            value: 98,
+            name: '合格'
+        }, {
+            value: 4,
+            name: '不合格'
+        }]
+    }]
+});
+
+var dataStyle = {
+    normal: {
+        label: {
+            show: false
+        },
+        labelLine: {
+            show: false
+        }
+    }
+};
+
+var placeHolderStyle = {
+    normal: {
+        color: 'rgba(0,0,0,0)',
+        label: {
+            show: false
+        },
+        labelLine: {
+            show: false
+        }
+    },
+    emphasis: {
+        color: 'rgba(0,0,0,0)'
+    }
+};
+
+
