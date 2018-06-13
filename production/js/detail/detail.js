71d845a0bf9df8aabf780c29311482c885c86409
161250059 <161250059@smail.nju.edu.cn>
2018-06-13 17:03:09 +0800

    Merge branch 'master' of https://github.com/YottaLee/BlockChainForMedicine
    # Conflicts:
    #	production/AccordingFindingConditionDisplay.html
    #	production/blockBrowser.html

--- production/js/detail/detail.js
+++ production/js/detail/detail.js
@@ -10,7 +10,7 @@ if (hash =="2"){
     var node = "种植采收";
     var step = "种植";
     var pici = "20180424001";
-    var info = "山东临沂平邑金银花种植基地";
+    var info = "宁夏中宁宁安堡种植基地";
     document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
@@ -67,7 +67,7 @@ else if(hash =="6"){
     var node = "种植采收";
     var step = "种植";
     var pici = "20180424001";
-    var info = "山东临沂平邑金银花种植基地";
+    var info = "宁夏中宁宁安堡种植基地";
     document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
