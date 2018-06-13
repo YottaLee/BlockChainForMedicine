b7245ffa5ff1dc7d52aaa8160a658b07ca20ad41
161250059 <161250059@smail.nju.edu.cn>
2018-06-13 17:15:54 +0800

    Merge branch 'master' of https://github.com/YottaLee/BlockChainForMedicine
    # Conflicts:
    #	production/AccordingFindingConditionDisplay.html
    #	production/blockBrowser.html

--- production/js/detail/detail.js
+++ production/js/detail/detail.js
@@ -11,7 +11,7 @@ if (hash =="2"){
     var step = "种植";
     var pici = "20180424001";
     var info = "宁夏中宁宁安堡种植基地";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "00fb78c6-542f-4aa4-80ff-fa3d7474f615"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -22,7 +22,7 @@ else if(hash =="3"){
     var step = "初加工";
     var pici = "20180523001";
     var info = "山东临沂平邑金银花初加工基地";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "2dfa6562-91d5-48f7-8e44-ec529e6a26a5"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -34,7 +34,7 @@ else if(hash =="4"){
     var step = "抽样检验";
     var pici = "20180524003";
     var info = "山东药监局";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "d346f791-05b2-42b9-a6dd-05fdadd7fb66"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -46,7 +46,7 @@ else if(hash =="5"){
     var step = "初加工";
     var pici = "20180523001";
     var info = "山东临沂平邑金银花初加工基地";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "adc5eca8-98ba-4f7b-b70e-423a456344e0"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -57,7 +57,7 @@ else if(hash =="6"){
     var step = "抽样检验";
     var pici = "20180524003";
     var info = "山东药监局";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "b6988e9a-2804-47f7-9884-0addd8f0c4f6"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -68,7 +68,7 @@ else if(hash =="6"){
     var step = "种植";
     var pici = "20180424001";
     var info = "宁夏中宁宁安堡种植基地";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "a6188db3-4750-4b3e-aa06-7c77c255e834"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
@@ -79,7 +79,7 @@ else{
     var step = "初加工";
     var pici = "20180523001";
     var info = "山东临沂平邑金银花初加工基地";
-    document.getElementById("hash_value").innerHTML = "哈希值: " + "353532e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8"+hash.substr(3);
+    document.getElementById("hash_value").innerHTML = "哈希值: " + "a6decd51-2cf1-45f0-9291-44ca3b99b05b"+hash.substr(3);
     document.getElementById("peer_name").innerHTML = node;
     document.getElementById("link_name").innerHTML = step;
     document.getElementById("batch_name").innerHTML = pici;
